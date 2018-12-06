087046ea79218b87644ec83e83b644da3a0db086
Ishaan <ishaanpaul98@gmail.com>
2018-12-06 05:02:10 -0500

    Fixed small bugs

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require ("express")
 var app = express();
 var bodyparser = require("body-parser");
 app.set("view engine","ejs");
-
+var spots = [
+		{name: "Hicks", image: "https://farm6.staticflickr.com/5125/13548348064_ba400533ab.jpg"},
+		{name: "WALC", image: "https://pixabay.com/get/e83db5072df2033ed1584d05fb1d4e97e07ee3d21cac104491f4c171afeeb3b8_340.jpg" },
+		{name: "Lawson", image: "https://www.photosforclass.com/download/flickr-2480920040"}
+	]
 app.use(bodyparser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
@@ -10,11 +14,7 @@ app.get("/",function(req,res){
 });
 
 app.get("/hotspots", function(req, res){
-	var spots = [
-		{name: "Hicks", image: "https://farm6.staticflickr.com/5125/13548348064_ba400533ab.jpg"},
-		{name: "WALC", image: "https://pixabay.com/get/e83db5072df2033ed1584d05fb1d4e97e07ee3d21cac104491f4c171afeeb3b8_340.jpg" },
-		{name: "Lawson", image: "https://www.photosforclass.com/download/flickr-2480920040"}
-	]
+	
 	res.render("hotspots", {spots:spots});
 });
 
@@ -22,8 +22,12 @@ app.get("/hotspots/new", function(req, res){
 	res.render("new.ejs");
 });
 
-app.post("/hotspots",function(req, res){
-
+app.post("/hotspots", function(req, res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var newone = {name:name, image:image};
+	spots.push(newone);
+	res.redirect("/hotspots");
 });
 
 app.listen(3000,function(){
