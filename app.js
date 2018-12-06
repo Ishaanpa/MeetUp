6eba4b6ed91f88efba7b0cdf407eace2764ba60a
Ishaan <ishaanpaul98@gmail.com>
2018-12-06 06:49:42 -0500

    Changed port on app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require ("express")
 var app = express();
-var port = process.env.PORT || 3333;
+var port = process.env.PORT || 3000;
 var bodyparser = require("body-parser");
 app.set("view engine","ejs");
 var spots = [
 		{name: "Hicks", image: "https://farm6.staticflickr.com/5125/13548348064_ba400533ab.jpg"},
 		{name: "WALC", image: "https://pixabay.com/get/e83db5072df2033ed1584d05fb1d4e97e07ee3d21cac104491f4c171afeeb3b8_340.jpg" },
-		{name: "Lawson", image: "https://www.photosforclass.com/download/flickr-2480920040"},
+		{name: "Lawson", image: "https://farm8.staticflickr.com/7683/16713808824_410320fc77.jpg"},
 		{name: "Hicks", image: "https://farm6.staticflickr.com/5125/13548348064_ba400533ab.jpg"},
 		{name: "WALC", image: "https://pixabay.com/get/e83db5072df2033ed1584d05fb1d4e97e07ee3d21cac104491f4c171afeeb3b8_340.jpg" },
-		{name: "Lawson", image: "https://www.photosforclass.com/download/flickr-2480920040"}
+		{name: "Lawson", image: "https://farm8.staticflickr.com/7683/16713808824_410320fc77.jpg"}
 	]
 app.use(bodyparser.urlencoded({extended: true}));
 
